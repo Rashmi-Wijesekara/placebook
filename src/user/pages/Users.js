d4ee59dee71bbf318de6373b22c81d62efbed826
Rashmi-Wijesekara <rashmiwijesekara1999@gmail.com>
2022-07-01 14:42:11 +0530

    package json updated & fixed slow image loading by compressed

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -8,28 +8,28 @@ const Users = () => {
 			id: 1,
 			name: "John Smith",
 			image:
-				"https://images.unsplash.com/photo-1584999734482-0361aecad844",
+				"https://images.unsplash.com/photo-1584999734482-0361aecad844?auto=compress",
 			places: 3,
 		},
 		{
 			id: 2,
 			name: "Daniel Scott",
 			image:
-				"https://images.unsplash.com/photo-1522075469751-3a6694fb2f61",
+				"https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?auto=compress",
 			places: 9,
 		},
 		{
 			id: 3,
 			name: "Susan Robinson",
 			image:
-				"https://images.unsplash.com/photo-1495580621852-5de0cc907d2f",
+				"https://images.unsplash.com/photo-1495580621852-5de0cc907d2f?auto=compress",
 			places: 6,
 		},
 		{
 			id: 4,
 			name: "Robert Martin",
 			image:
-				"https://images.unsplash.com/photo-1541534401786-2077eed87a74",
+				"https://images.unsplash.com/photo-1541534401786-2077eed87a74?auto=compress",
 			places: 12,
 		},
 	];
