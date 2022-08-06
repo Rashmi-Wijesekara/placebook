2e02b1c7c194a6ce8624b4b982e0c38793dc34f6
Rashmi <rashmiwijesekara1999@gmail.com>
2022-08-06 23:41:49 +0530

    input validation added

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -1,10 +1,38 @@
-import React from "react";
-import Input from "../../shared/components/FormElements/Input"
+import React, { useCallback } from "react";
+import Input from "../../shared/components/FormElements/Input";
+import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from "../../shared/utils/validators";
 
 const NewPlace = () => {
+	const titleInputHandler = useCallback(
+		(id, value, isValid) => {},
+		[] // this will run only at the beginning
+	);
+
+	const descriptionInputHandler = useCallback(
+		(id, value, isValid) => {},
+		[]
+	);
+
 	return (
 		<form className="">
-			<Input element="input" type="text" label="Title" />
+			<Input
+				id="title"
+				element="input"
+				type="text"
+				label="Title"
+				errorText="Please enter a valid title"
+				validators={[VALIDATOR_REQUIRE()]}
+				onInput={titleInputHandler}
+			/>
+
+			<Input
+				id="description"
+				element="textarea"
+				label="Description"
+				errorText="Please enter a valid description (at least 5 characters)"
+				validators={[VALIDATOR_MINLENGTH(5)]}
+				onInput={descriptionInputHandler}
+			/>
 		</form>
 	);
 };
--- frontend/src/shared/components/FormElements/Input.js
+++ frontend/src/shared/components/FormElements/Input.js
@@ -1,13 +1,19 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
+import {validate} from "../../utils/validators"
 
 const inputReducer = (state, action) => {
 	switch (action.type) {
 		case "CHANGE":
 			return {
 				...state,
-				value: action.value,
-				isValid: true,
+				value: action.val,
+				isValid: validate(action.val, action.validators)
 			};
+		case "TOUCH":
+			return {
+				...state,
+				isTouched: true
+			}
 		default:
 			return state;
 	}
@@ -17,7 +23,29 @@ const Input = (props) => {
 	const [inputState, dispatch] = useReducer(inputReducer, {
 		value: "",
 		isValid: false,
+		isTouched: false
 	});
+
+	const {id, onInput} = props;
+	const {value, isValid} = inputState;
+
+	useEffect(() => {
+		onInput(id, value, isValid);
+	}, [id, value, isValid, onInput]);
+
+	const changeHandler = (event) => {
+		dispatch({
+			type: 'CHANGE',
+			val: event.target.value,
+			validators: props.validators
+		})
+	}
+
+	const touchHandler = (event) => {
+		dispatch({
+			type: 'TOUCH'
+		})
+	}
 
 	const element =
 		props.element === "input" ? (
@@ -25,21 +53,31 @@ const Input = (props) => {
 				id={props.id}
 				type={props.type}
 				placeholder={props.placeholder}
-				className="border-2 border-slate-200 mx-5 px-2 py-1 rounded-xl"
+				onChange={changeHandler}
+				value={inputState.value}
+				onBlur={touchHandler}
+				className={`border-2 border-slate-200 mx-5 px-2 py-1 rounded-xl ${
+					inputState.isTouched && 'bg-red-200 border-red-400'
+				}`}
 			/>
 		) : (
 			<textarea
 				id={props.id}
 				rows={props.rows || 3}
+				onChange={changeHandler}
+				value={inputState.value}
+				onBlur={touchHandler}
 				className="border-2 border-slate-200 rounded-xl font-medium"
 			/>
 		);
+
 	return (
 		<div
 			className={`shadow-lg w-fit  rounded-xl px-10 py-12 mx-auto my-5 font-mono font-semibold`}
 		>
 			<label htmlFor={props.id}>{props.label}</label>
 			{element}
+			{!inputState.isValid && inputState.isTouched && <p className="text-red-500 text-xs text-right my-2">{props.errorText}</p>}
 		</div>
 	);
 };
